0df7d93925a5824d232e9a58fac8d80886a0c605
Alexandros Oikonomou <alexandros98g7@gmail.com>
2023-02-22 17:07:52 +0200

    Update setup_f1_auth.bash

--- setup_f1_auth.bash
+++ setup_f1_auth.bash
@@ -5,6 +5,8 @@ echo " >>>>>> This is the beginning of the script for the installation of ROS an
 echo " "
 echo " >>>>>> Setup source list, keys"
 echo " "
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
 sudo apt -y install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt -y update
