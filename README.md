3c32c58aae53e7fe0b29963ec9a99f4d9e8459ff
Alexandros Oikonomou <alexandros98g7@gmail.com>
2023-02-22 15:42:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+AUTh F1TENTH Workshop 22-February-2023
+===
+
+Installation Steps:
+
+1) Download **Ubuntu 18.04** ISO file from [(https://releases.ubuntu.com/18.04.6/](https://releases.ubuntu.com/18.04.6/)
+(Desktop, ISO image, 64bit)
+
+2) Download and Install a **Virtual Machine**, e.g. Vmware Workstation [Download for Windows](https://www.vmware.com/products/workstation-pro/workstation-pro-evaluation.html)
+
+3) Create new virtual machine (use wizard).
+
+> Choose Typical, ISO, Credentials, 20Gb.
+
+4) Download script via ``git clone https://github.com/alexoiko/AUTh_F1Tenth_Workshop.git``
+
+5) Open terminal, e.g. Ctrl+Alt+T shortcut
+
+6) Change permission ``chmod +x setup_f1_auth.bash``
+
+7) Run Script, e.g. `bash setup_f1_auth.bash``
---
+++ setup_f1_auth.bash
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo " "
+echo " >>>>>> This is the beginning of the script for the installation of ROS and F1/10 simulator."
+
+ 
+
+# ROS installation
+
+echo " "
+echo " >>>>>> Setup source list, keys"
+echo " "
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt -y install curl 
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo  apt-key add -
+sudo apt -y update
+
+echo " "
+echo " >>>>>> Install ROS Melodic & Dependencies"
+echo " "
+
+sudo  apt -y install ros-melodic-desktop-full
+ 
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential #python-roslaunch
+
+sudo rosdep init
+rosdep update
+
+echo " "
+echo " >>>>>> Install F1/10 Simulator"
+echo " "
+
+sudo apt -y install ros-melodic-tf2-geometry-msgs ros-melodic-ackermann-msgs ros-melodic-joy ros-melodic-map-server
+##turtesim — already installed
+#sudo apt -y install ros-melodic-turtlesim
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+
+git clone https://github.com/f1tenth/f1tenth_simulator.git
+#git clone https://github.com/mit-racecar/particle_filter
+#git clone https://github.com/kctess5/range_libc
+#git clone https://github.com/tu-darmstadt-ros-pkg/hector_slam
+
+cd ~/catkin_ws
+catkin_make #install
+source devel/setup.bash
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+ 
+
+echo " "
+echo "Run F1/10 Simulator"
+echo " "
+
+roslaunch f1tenth_simulator simulator.launch
